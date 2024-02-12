3f9022ca242e09f0eb74c773d52ffa3c4bbc9862
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-12 02:01:24 -0800

    9.15

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,25 @@ export type Course = {
     name: string;
     exerciseCount: number;
 };
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+}
+
+interface CoursePartBasic extends CoursePartBase {
+    description: string;
+    kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartBase {
+    description: string;
+    backgroundMaterial: string;
+    kind: 'background';
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
